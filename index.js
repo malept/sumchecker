0496c6af36a0d85da9be1ba10a18984403c2183e
Mark Lee <sumchecker@lazymalevolence.com>
2019-04-20 10:08:35 -0700

    chore: add more eslint rules

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-'use strict'
-
 const debug = require('debug')('sumchecker')
 const crypto = require('crypto')
 const fs = require('fs')
